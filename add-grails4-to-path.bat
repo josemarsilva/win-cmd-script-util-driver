c11bc0cb71b7645ea2ed1f803ab421c8d0faac06
josemarsilva <josemar.furegatti@gmail.com>
2020-05-20 08:42:34 -0300

    grails4

--- add-grails4-to-path.bat
+++ add-grails4-to-path.bat
@@ -28,13 +28,7 @@ IF NOT EXIST %PROGRAM_PATH%\%PROGRAM_NAME% (
 	ECHO        - Install %PROGRAM_NAME% into '%PROGRAM_PATH%' ...
 	ECHO          ... or define environment variable 'GRAILS_BIN_PATH'
 	ECHO.
-) ELSE (
-	ECHO.
-	REM ECHO Setting '%PROGRAM_NAME%' with Java JDK 1.7 to PATH environment ...
-	ECHO.
 )
 
-REM SET JAVA_HOME=C:\Program Files\Java\jdk1.7.0_80
-REM SET PATH=%SystemRoot%\system32;%SystemRoot%;%SystemRoot%\System32\Wbem;%SYSTEMROOT%\System32\WindowsPowerShell\v1.0\;%SYSTEMROOT%\System32\OpenSSH\;C:\Program Files\Git\cmd;%JAVA_HOME%\bin;%PROGRAM_PATH%;%M2_HOME%\bin;C:\Program Files\nodejs\;C:\Program Files\Microsoft VS Code\bin;C:\Program Files\Microsoft SQL Server\Client SDK\ODBC\110\Tools\Binn\;C:\PROGRA~2\Microsoft SQL Server\120\Tools\Binn\;C:\Program Files\Microsoft SQL Server\120\Tools\Binn\;C:\Program Files\Microsoft SQL Server\120\DTS\Binn\;C:\PROGRA~2\Microsoft SQL Server\120\Tools\Binn\ManagementStudio\;C:\PROGRA~2\Microsoft SQL Server\120\DTS\Binn\;C:\Users\Inmetrics\Documents\win-cmd-script-util-driver;
-ECHO JAVA_HOME=%JAVA_HOME%
+SET PATH=%PATH%;%PROGRAM_PATH%
 ECHO PATH=%PATH%

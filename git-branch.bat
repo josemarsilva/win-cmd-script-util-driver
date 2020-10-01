d9767df2b1092231c47c1eeba99381802b8fa681
husrpsap <husrpsap>
2020-10-01 11:44:51 -0300

    git-branch.bat: parametro de branch

--- git-branch.bat
+++ git-branch.bat
@@ -11,8 +11,19 @@ FOR /f "tokens=*" %%G IN ('DIR /a:d /b /o:n /o:-d') DO (
   CD %%G
   CD
   IF EXIST ".git\" (
-    ECHO git branch
-    git branch
+    ECHO git branch --list
+    git branch --list
+    IF NOT "%1" == "" (
+      git branch --list %1 > ..\git-branch.tmp
+	  SET STR= < ..\git-branch.tmp
+      DEL /Q ..\git-branch.tmp
+      IF NOT "%STR" == "" (
+        ECHO git checkout %1 ; git pull ; git status
+        git checkout %1
+        git git pull
+        git status
+      )
+    )
   ) ELSE (
     ECHO It's not a git repository ... skipped !
   )
